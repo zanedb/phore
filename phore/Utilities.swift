d80a56e87ffe27beb6b5e707ccc3f34ac4dde759
Zane <github@zane.sh>
2024-08-10 17:17:51 -0700

    Add app util

--- phore/Utilities.swift
+++ phore/Utilities.swift
@@ -2,7 +2,7 @@
 //  Utilities.swift
 //  phore
 //
-//  Created by Zane on 1/23/23.
+//  Created by Zane on 8/10/24.
 //
 
 import Foundation
@@ -12,4 +12,52 @@ struct VisualEffectView: UIViewRepresentable {
     var effect: UIVisualEffect?
     func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
     func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
+}
+
+// https://github.com/sindresorhus/Blear/blob/5326e9b891e609c23641d43b966501afe21019ca/Blear/Utilities.swift#L190
+enum SSApp {
+    static let idString = Bundle.main.bundleIdentifier!
+    static let name = Bundle.main.object(forInfoDictionaryKey: kCFBundleNameKey as String) as! String
+    static let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+    static let build = Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
+    static let versionWithBuild = "\(version) (\(build))"
+    // static let rootName = Bundle.app.object(forInfoDictionaryKey: kCFBundleNameKey as String) as! String
+
+    static let isFirstLaunch: Bool = {
+        let key = "__hasLaunched__"
+
+        if UserDefaults.standard.bool(forKey: key) {
+            return false
+        } else {
+            UserDefaults.standard.set(true, forKey: key)
+            return true
+        }
+    }()
+}
+
+// https://github.com/sindresorhus/Blear/blob/5326e9b891e609c23641d43b966501afe21019ca/Blear/Utilities.swift#L1881
+extension Text {
+    /**
+    By default, `Text` only accepts Markdown when using a string literal or explicitly passing a `LocalizedStringKey`. That's a bit too magic and I prefer an explicit initializer.
+    */
+    init(markdown: String) {
+        self.init(LocalizedStringKey(markdown))
+    }
+}
+
+// https://github.com/sindresorhus/Blear/blob/5326e9b891e609c23641d43b966501afe21019ca/Blear/Utilities.swift#L1891
+extension View {
+    /**
+    Fills the frame.
+    */
+    func fillFrame(
+        _ axis: Axis.Set = [.horizontal, .vertical],
+        alignment: Alignment = .center
+    ) -> some View {
+        frame(
+            maxWidth: axis.contains(.horizontal) ? .infinity : nil,
+            maxHeight: axis.contains(.vertical) ? .infinity : nil,
+            alignment: alignment
+        )
+    }
 }
